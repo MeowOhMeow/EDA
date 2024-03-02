5cec96b48009d48acee20e4e590f63b0c264e7b7
MeowOhMeow <abc22681157@gmail.com>
2024-03-02 18:56:44 +0800

    no rick roll

--- PA1/main.cpp
+++ PA1/main.cpp
@@ -2,22 +2,11 @@
 #include "utility.h"
 #include "debug.h"
 #include <iostream>
-#include <cstdlib>
-#include <string>
-
-#ifdef _WIN32
-string openCommand = "start ";
-#else
-string openCommand = "xdg-open ";
-#endif
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    string url = "https://youtu.be/dQw4w9WgXcQ?si=kKf0nsl9uuBP3TZk";
-    system((openCommand + url).c_str());
-
     Assert(argc == 3, "Usage: %s <input file> <output file>", argv[0]);
     Generator g(argv[1]);
     g.generate();
